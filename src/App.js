52c35423d2e9b46e42725c8a3a3928b33c1e06b8
GimYoungPhil <youngphil.gim@gmail.com>
2020-10-16 14:22:47 +0900

    add BooksList

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
+
 import './App.css';
+import BooksList from './features/books/BooksList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h5>The Books</h5>
       </header>
+      <BooksList/>
     </div>
   );
 }
---
+++ src/features/books/BooksList.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { selectAllBooks, fetchBooks } from './booksSlice';
+
+const BookExcerpt = ({ book }) => {
+  return (
+    <article className="book-excerpt" key={book.id}>
+      <h3>{book.title}</h3>
+      <p>{book.author}</p>
+    </article>
+  );
+};
+
+const BooksList = () => {
+  const dispatch = useDispatch();
+
+  const books = useSelector(selectAllBooks);
+  const status = useSelector(state => state.books.status);
+  const error = useSelector(state => state.books.error);
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchBooks());
+    }
+  }, [status, dispatch]);
+
+  let content;
+
+  if (status === 'loading') {
+    content = <div>Loading...</div>;
+  } else if (status === 'succeeded') {
+    content = books.map(book => (
+      <BookExcerpt key={book.id} book={book} />
+    ));
+  } else if (status === 'failed') {
+    content = <div>{error}</div>
+  }
+
+  return (
+    <section>
+      <h3>Books</h3>
+      {content}
+    </section>
+  )
+};
+
+export default BooksList;
